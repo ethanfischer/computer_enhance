fe18121d7219242b0cdcf58a839bfdfd3b4435e4
Ethan <ethan.f.fischer@gmail.com>
2023-03-06 09:59:39 -0600

    cleanup a little

--- perfaware/part1/homework/InstructionDecoding.cs
+++ perfaware/part1/homework/InstructionDecoding.cs
@@ -3,148 +3,149 @@
     private static void Main(string[] args)
     {
         var bytes = File.ReadAllBytes("/home/ethan/repos/computer_enhance/perfaware/part1/listing_0038_many_register_mov");
-        for(var i = 0; i < bytes.Length - 1; i+=2)
+        for (var i = 0; i < bytes.Length - 1; i += 2)
         {
-            DecodeBytes(bytes[i], bytes[i+1]);
+            DecodeBytes(bytes[i], bytes[i + 1]);
         }
 
-        string GetOpCode(string firstByte)
+    }
+
+    static void DecodeBytes(byte firstByte, byte secondByte)
+    {
+        var binary = Convert.ToString(firstByte, toBase: 2);
+        var opCode = GetOpCode(binary);
+        var isDBit = IsDBit(firstByte);
+
+        var firstRegister = GetFirstRegister(firstByte, secondByte);
+        var secondRegister = GetSecondRegister(firstByte, secondByte);
+
+        if (isDBit)
         {
-            if (firstByte.StartsWith("100010"))
+            Console.WriteLine($"{opCode} {firstRegister}, {secondRegister}");
+        }
+        else
+        {
+            Console.WriteLine($"{opCode} {secondRegister}, {firstRegister}");
+        }
+    }
+
+    static string GetOpCode(string firstByte)
+    {
+        if (firstByte.StartsWith("100010"))
+        {
+            return "mov";
+        }
+        else
+        {
+            return "unknown";
+        }
+    }
+
+    static string GetFirstRegister(byte firstByte, byte secondByte)
+    {
+        var firstRegisterCode = GetFirstRegisterCode(secondByte);
+
+        if (IsW(firstByte))
+        {
+            switch (firstRegisterCode)
             {
-                return "mov";
-            }
-            else
-            {
-                return "unknown";
+                case 0b_00_000_000:
+                    return "ax";
+                case 0b_00_001_000:
+                    return "cx";
+                case 0b_00_010_000:
+                    return "dx";
+                case 0b_00_011_000:
+                    return "bx";
+                case 0b_00_100_000:
+                    return "sp";
+                case 0b_00_101_000:
+                    return "bp";
+                case 0b_00_110_000:
+                    return "si";
+                case 0b_00_111_000:
+                    return "di";
+                default:
+                    return "";
             }
         }
-
-        string GetFirstRegister(byte firstByte, byte secondByte)
+        else
         {
-            var firstRegisterCode = GetFirstRegisterCode(secondByte);
-
-            if (IsW(firstByte))
+            switch (firstRegisterCode)
             {
-                switch (firstRegisterCode)
-                {
-                    case 0b_00_000_000:
-                        return "ax";
-                    case 0b_00_001_000:
-                        return "cx";
-                    case 0b_00_010_000:
-                        return "dx";
-                    case 0b_00_011_000:
-                        return "bx";
-                    case 0b_00_100_000:
-                        return "sp";
-                    case 0b_00_101_000:
-                        return "bp";
-                    case 0b_00_110_000:
-                        return "si";
-                    case 0b_00_111_000:
-                        return "di";
-                    default:
-                        return "";
-                }
-            }
-            else
-            {
-                switch (firstRegisterCode)
-                {
-                    case 0b_00_000_000:
-                        return "al";
-                    case 0b_00_001_000:
-                        return "cl";
-                    case 0b_00_010_000:
-                        return "dl";
-                    case 0b_00_011_000:
-                        return "bl";
-                    case 0b_00_100_000:
-                        return "ah";
-                    case 0b_00_101_000:
-                        return "ch";
-                    case 0b_00_110_000:
-                        return "dh";
-                    case 0b_00_111_000:
-                        return "bh";
-                    default:
-                        return "";
-                }
+                case 0b_00_000_000:
+                    return "al";
+                case 0b_00_001_000:
+                    return "cl";
+                case 0b_00_010_000:
+                    return "dl";
+                case 0b_00_011_000:
+                    return "bl";
+                case 0b_00_100_000:
+                    return "ah";
+                case 0b_00_101_000:
+                    return "ch";
+                case 0b_00_110_000:
+                    return "dh";
+                case 0b_00_111_000:
+                    return "bh";
+                default:
+                    return "";
             }
         }
+    }
 
 
-        string GetSecondRegister(byte firstByte, byte secondByte)
+    static string GetSecondRegister(byte firstByte, byte secondByte)
+    {
+        var secondRegisterCode = GetSecondRegisterCode(secondByte);
+
+        if (IsW(firstByte))
         {
-            var secondRegisterCode = GetSecondRegisterCode(secondByte);
-
-            if (IsW(firstByte))
+            switch (secondRegisterCode)
             {
-                switch (secondRegisterCode)
-                {
-                    case 0b_00_000_000:
-                        return "ax";
-                    case 0b_00_000_001:
-                        return "cx";
-                    case 0b_00_000_010:
-                        return "dx";
-                    case 0b_00_000_011:
-                        return "bx";
-                    case 0b_00_000_100:
-                        return "sp";
-                    case 0b_00_000_101:
-                        return "bp";
-                    case 0b_00_000_110:
-                        return "si";
-                    case 0b_00_000_111:
-                        return "di";
-                    default:
-                        return "ERROR";
-                }
-            }
-            else
-            {
-                switch (secondRegisterCode)
-                {
-                    case 0b_00_000_000:
-                        return "al";
-                    case 0b_00_000_001:
-                        return "cl";
-                    case 0b_00_000_010:
-                        return "dl";
-                    case 0b_00_000_011:
-                        return "bl";
-                    case 0b_00_000_100:
-                        return "ah";
-                    case 0b_00_000_101:
-                        return "ch";
-                    case 0b_00_000_110:
-                        return "dh";
-                    case 0b_00_000_111:
-                        return "bh";
-                    default:
-                        return "ERROR";
-                }
+                case 0b_00_000_000:
+                    return "ax";
+                case 0b_00_000_001:
+                    return "cx";
+                case 0b_00_000_010:
+                    return "dx";
+                case 0b_00_000_011:
+                    return "bx";
+                case 0b_00_000_100:
+                    return "sp";
+                case 0b_00_000_101:
+                    return "bp";
+                case 0b_00_000_110:
+                    return "si";
+                case 0b_00_000_111:
+                    return "di";
+                default:
+                    return "ERROR";
             }
         }
-
-        void DecodeBytes(byte firstByte, byte secondByte)
+        else
         {
-            var binary = Convert.ToString(firstByte, toBase: 2);
-            var opCode = GetOpCode(binary);
-            var isDBit = IsDBit(firstByte);
-
-            var firstRegister = GetFirstRegister(firstByte, secondByte);
-            var secondRegister = GetSecondRegister(firstByte, secondByte);
-
-            if (isDBit)
+            switch (secondRegisterCode)
             {
-                Console.WriteLine($"{opCode} {firstRegister}, {secondRegister}");
-            }
-            else
-            {
-                Console.WriteLine($"{opCode} {secondRegister}, {firstRegister}");
+                case 0b_00_000_000:
+                    return "al";
+                case 0b_00_000_001:
+                    return "cl";
+                case 0b_00_000_010:
+                    return "dl";
+                case 0b_00_000_011:
+                    return "bl";
+                case 0b_00_000_100:
+                    return "ah";
+                case 0b_00_000_101:
+                    return "ch";
+                case 0b_00_000_110:
+                    return "dh";
+                case 0b_00_000_111:
+                    return "bh";
+                default:
+                    return "ERROR";
             }
         }
     }
