e31c4c7770f05674bc58560d0f8ab297f21e60fa
ethanfischer <ethan.fischer@incontextsolutions.com>
2023-05-18 14:24:35 +0800

    Listing 54 complete

--- perfaware/sim86/shared/contrib_csharp/sim86_test.cs
+++ perfaware/sim86/shared/contrib_csharp/sim86_test.cs
@@ -8,18 +8,20 @@ internal class Program
     private static byte[] _memory = new byte[1_048_576]; //1MB
     private static int[] _registers = new int[9];
     private static ArithmeticFlags _arithmeticFlags;
+
     private static void Main(string[] args)
     {
-        var exampleDisassembly = File.ReadAllBytes("/Users/ethanfischer/Repos/computer_enhance/perfaware/part1/listing_0052_memory_add_loop");
+        var lastInstructionIndex = ReadFileContentsIntoMemory();
+
         Console.WriteLine($"Sim86 Version: {Sim86.GetVersion()}");
 
         var table = Sim86.Get8086InstructionTable();
         Console.WriteLine($"8086 Instruction Instruction Encoding Count: {table.Encodings.Length}");
 
         _registers.SetIP(0);
-        while (_registers[Sim86.IP] < exampleDisassembly.Length)
+        while (_registers[Sim86.IP] < lastInstructionIndex)
         {
-            var decoded = Sim86.Decode8086Instruction(exampleDisassembly.AsSpan().Slice(_registers[Sim86.IP]));
+            var decoded = Sim86.Decode8086Instruction(_memory.AsSpan().Slice(_registers[Sim86.IP]));
             if (decoded.Op == Sim86.OperationType.mov)
             {
                 Mov.Handle(decoded, _registers, _memory);
@@ -52,12 +54,34 @@ internal class Program
         for (var i = 0; i < _registers.Length; i++)
         {
             var value = _registers[i];
-            if(value == 0) continue;
+            if (value == 0) continue;
 
             Console.WriteLine($"\t{(RegisterId)i}: 0x{value.Hex()} ({value})");
         }
+
         Console.WriteLine("");
-        Console.WriteLine($"flags: {InstructionUtils.GetZeroFlagText(_arithmeticFlags)}{InstructionUtils.GetSignedFlagText(_arithmeticFlags)}");
+        Console.WriteLine(
+            $"flags: {InstructionUtils.GetZeroFlagText(_arithmeticFlags)}{InstructionUtils.GetSignedFlagText(_arithmeticFlags)}");
+
+        File.WriteAllBytes(
+            "/Users/ethanfischer/Repos/computer_enhance/perfaware/sim86/shared/contrib_csharp/memory_output.data",
+            _memory);
+    }
+
+    private static int ReadFileContentsIntoMemory()
+    {
+        const string filePath = "/Users/ethanfischer/Repos/computer_enhance/perfaware/part1/listing_0054_draw_rectangle";
+        using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+        int bytesRead;
+        var offset = 0;
+        while ((bytesRead = fileStream.Read(_memory, offset, _memory.Length - offset)) > 0)
+        {
+            offset += bytesRead;
+            if (offset >= _memory.Length)
+                break; // Reached the end of the _memory array
+        }
+
+        return offset;
     }
 }
 
