a731a2fcb0e76d7de7f2684846f055949ac1b630
Ethan Fischer <Ethan.Fischer@incontextsolutions.com>
2023-06-19 03:08:41 -0500

    Implement RDTSC using c and p/invoke. Listing 73 complete

--- perfaware/part2/HaversineProcessor/HaversineProcessor/Program.cs
+++ perfaware/part2/HaversineProcessor/HaversineProcessor/Program.cs
@@ -5,19 +5,21 @@ using JsonGenerator;
 
 internal class Program
 {
-    public static void Main(string[] args)
-    {
-        var jsonBytes = File.ReadAllBytes("C:/Users/ethan.fischer/GitProjects/computer_enhance/perfaware/part2/JsonGeneration/JsonGenerator/JsonGenerator/data.json");
-        var pairs = JsonParser.Deserialize(jsonBytes);
-        var answer = File.ReadAllText("C:/Users/ethan.fischer/GitProjects/computer_enhance/perfaware/part2/JsonGeneration/JsonGenerator/JsonGenerator/answer.txt");
-        var count = pairs.Count;
-        var sum = 0.00d;
-        foreach(var pair in pairs)
-        {
-            sum += Haversine.ReferenceHaversine(pair.X0, pair.Y0, pair.X1, pair.Y1);
-        }
-        
-        Console.WriteLine($"Answer {sum/count}");
-        Console.WriteLine($"Expected answer {answer}");
-    }
+    // public static void Main(string[] args)
+    // {
+    //     var t = Rdtsc.Timestamp();
+    //     Console.WriteLine(t);
+    //     var jsonBytes = File.ReadAllBytes("C:/Users/ethan.fischer/GitProjects/computer_enhance/perfaware/part2/JsonGeneration/JsonGenerator/JsonGenerator/data.json");
+    //     var pairs = JsonParser.Deserialize(jsonBytes);
+    //     var answer = File.ReadAllText("C:/Users/ethan.fischer/GitProjects/computer_enhance/perfaware/part2/JsonGeneration/JsonGenerator/JsonGenerator/answer.txt");
+    //     var count = pairs.Count;
+    //     var sum = 0.00d;
+    //     foreach(var pair in pairs)
+    //     {
+    //         sum += Haversine.ReferenceHaversine(pair.X0, pair.Y0, pair.X1, pair.Y1);
+    //     }
+    //     
+    //     Console.WriteLine($"Answer {sum/count}");
+    //     Console.WriteLine($"Expected answer {answer}");
+    // }
 }
---
+++ perfaware/part2/HaversineProcessor/HaversineProcessor/TimerHomework.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace JsonGenerator;
+
+public class TimerHomework
+{
+    [DllImport("kernel32.dll")]
+    public static extern bool QueryPerformanceFrequency(out ulong frequency);
+
+    [DllImport("kernel32.dll")]
+    public static extern bool QueryPerformanceCounter(out ulong counter);
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("How many milliseconds to wait?");
+        var input = Console.ReadLine();
+        ulong MillisecondsToWait = 1000;
+        if (input != string.Empty)
+        {
+            MillisecondsToWait = ulong.Parse(input);
+        }
+
+        var OSFreq = GetOSTimerFreq();
+        Console.WriteLine("    OS Freq: {0}", OSFreq);
+
+        var CPUStart = ReadCPUTimer();
+        var OSStart = ReadOSTimer();
+        ulong OSEnd = 0;
+        ulong OSElapsed = 0;
+        ulong CPUEnd = 0;
+        ulong CPUElapsed = 0;
+        ulong OSWaitTime = OSFreq * MillisecondsToWait / 1000;
+        while (OSElapsed < OSWaitTime)
+        {
+            OSEnd = ReadOSTimer();
+            OSElapsed = OSEnd - OSStart;
+        }
+
+        CPUEnd = ReadCPUTimer();
+        CPUElapsed = CPUEnd - CPUStart;
+        ulong CPUFreq = 0;
+        if (OSElapsed > 0)
+        {
+            CPUFreq = OSFreq * CPUElapsed / OSElapsed;
+        }
+
+        Console.WriteLine("   OS Timer: {0} -> {1} = {2} elapsed", OSStart, OSEnd, OSElapsed);
+        Console.WriteLine(" OS Seconds: {0}", (ulong)OSElapsed / (ulong)OSFreq);
+        Console.WriteLine("   CPU Timer: {0} -> {1} = {2} elapsed", CPUStart, CPUEnd, CPUElapsed);
+        Console.WriteLine("   CPU Freq: {0} (guessed)", CPUFreq);
+
+        Main(null);
+    }
+
+    static ulong GetOSTimerFreq()
+    {
+        if (QueryPerformanceFrequency(out var frequency))
+        {
+            return frequency;
+        }
+
+        Console.WriteLine("ERROR: QueryPerformanceFrequency failed!");
+        return 0;
+    }
+
+    static ulong ReadOSTimer()
+    {
+        if (QueryPerformanceCounter(out var counter))
+        {
+            return counter;
+        }
+
+        Console.WriteLine("ERROR: QueryPerformanceCounter failed!");
+        return 0;
+    }
+
+    public static ulong ReadCPUTimer()
+    {
+        var result = rdtsc();
+        return result;
+    }
+
+    [DllImport("Rdtsc.dll")]
+    private static extern ulong rdtsc();
+}
