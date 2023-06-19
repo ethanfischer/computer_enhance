b51b7d6365c552b702f0ee66a2c5c3985814945b
Ethan Fischer <Ethan.Fischer@incontextsolutions.com>
2023-06-19 03:47:06 -0500

    Cleanup and use Stopwatch instead

---
+++ perfaware/part2/HaversineProcessor/HaversineProcessor/RdtscService.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+namespace JsonGenerator;
+
+public class RdtscService
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("How many milliseconds to wait?");
+        var input = Console.ReadLine();
+        long millisecondsToWait = 1000;
+        if (input != string.Empty)
+        {
+            millisecondsToWait = long.Parse(input);
+        }
+
+        EstimateCpuFrequency(millisecondsToWait);
+
+        Main(null);
+    }
+
+    static long ReadCpuTimer()
+    {
+        var result = rdtsc();
+        return (long)result;
+    }
+
+    [DllImport("Rdtsc.dll")]
+    public static extern ulong rdtsc();
+
+    public static long EstimateCpuFrequency(long millisecondsToWait)
+    {
+        var osFreq = Stopwatch.Frequency;
+        Console.WriteLine("    OS Freq: {0}", osFreq);
+
+        var cpuStart = ReadCpuTimer();
+        var osStart = Stopwatch.GetTimestamp();
+        var osEnd = 0L;
+        var osElapsed = 0L;
+        var osWaitTime = osFreq * millisecondsToWait / 1000;
+        while (osElapsed < osWaitTime)
+        {
+            osEnd = Stopwatch.GetTimestamp();
+            osElapsed = osEnd - osStart;
+        }
+
+        var cpuEnd = ReadCpuTimer();
+        var cpuElapsed = cpuEnd - cpuStart;
+        var cpuFreq = 0L;
+        if (osElapsed > 0)
+        {
+            cpuFreq = osFreq * cpuElapsed / osElapsed;
+        }
+
+        Console.WriteLine("   OS Timer: {0} -> {1} = {2} elapsed", osStart, osEnd, osElapsed);
+        Console.WriteLine(" OS Seconds: {0}", osElapsed / osFreq);
+        Console.WriteLine("   CPU Timer: {0} -> {1} = {2} elapsed", cpuStart, cpuEnd, cpuElapsed);
+        Console.WriteLine("   CPU Freq: {0} (guessed)", cpuFreq);
+
+        return cpuFreq;
+    }
+}
